d07614a9b044a39ffa4fa328d748038ef5e00008
Darknab <117382045+Darknab@users.noreply.github.com>
2024-06-11 16:33:50 +0100

    Set up images route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :images, only: [:show]
+  end
   root 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
